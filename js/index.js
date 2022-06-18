a1b47bb13c1780289823f68c6b0e6cb39bc20bef
timkwali <timkwalit@accessbankplc.com>
2022-06-18 12:07:54 +0100

    add more requests

--- js/index.js
+++ js/index.js
@@ -5,46 +5,61 @@ const { PORT } = process.env
 const port = process.env.PORT || PORT;
 const baseUrl = `http://localhost:${port}`
 
-const getBio = async () => {
+const getPortfolio = async () => {
     try {
-        let res = await axios.get(`${baseUrl}/bio`)
-        let bio = res.data
-        console.log(bio)
+        let res = await axios.get(`${baseUrl}/portfolio`)
+        let portfolio = res.data
+        console.log(portfolio)
     } catch (err) {
         console.error(err)
     }
 }
 
-const getAbout = async () => {
-    try {
-        let res = await axios.get(`${baseUrl}/about`)
-        let about = res.data
-        console.log(about)
-    } catch (err) {
-        console.error(err)
-    }
-}
+// const getBio = async () => {
+//     try {
+//         let res = await axios.get(`${baseUrl}/bio`)
+//         let bio = res.data
+//         console.log(bio)
+//     } catch (err) {
+//         console.error(err)
+//     }
+// }
 
-const getContact = async () => {
-    try {
-        let res = await axios.get(`${baseUrl}/contact`)
-        let contact = res.data
-        console.log(contact)
-    } catch (err) {
-        console.error(err)
-    }
-}
+// const getAbout = async () => {
+//     try {
+//         let res = await axios.get(`${baseUrl}/about`)
+//         let about = res.data
+//         console.log(about)
+//     } catch (err) {
+//         console.error(err)
+//     }
+// }
 
-let bio = getBio()
-let about = getAbout()
-let contact = getContact()
+// const getContact = async () => {
+//     try {
+//         let res = await axios.get(`${baseUrl}/contact`)
+//         let contact = res.data
+//         console.log(contact)
+//     } catch (err) {
+//         console.error(err)
+//     }
+// }
+
+let bioData = getPortfolio()[bio]
+let aboutData = getPortfolio()[about]
+let contactData = getPortfolio()[contact]
 
 const firstName = document.querySelector("#firstNmae")
 const lastName = document.querySelector("#lastNmae")
 const occupation = document.querySelector("#occupation")
 const stacks = document.querySelector("#stacks")
+const about = document.querySelector("#about")
+const email = document.querySelector("#email")
+const linkedIn = document.querySelector("#linkedIn")
+const twitter = document.querySelector("#twitter")
+
 let techStacks = ""
-bio.stacks.forEach(stack => {
+bioData.stacks.forEach(stack => {
     techStacks += `${stack}\n`
 })
 
@@ -53,3 +68,8 @@ lastName.textContent = `Last Name: ${bio[lastNmae]}`
 occupation.textContent = `Occupation: ${bio[occupation]}`
 stacks.textContent = `Tech Stacks: ${techStacks}`
 
+about.textContent = `About: ${aboutData}`
+
+email.textContent = `Email: ${contactData["email"]}`
+linkedIn.textContent = `LinkedIn: ${contactData["linkedIn"]}`
+twitter.textContent = `Twitter: ${contactData["twitter"]}`
